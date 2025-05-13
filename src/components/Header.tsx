7d1b2d7c97b1bbf601719056b0cd636195ee60b0
Jalter-ego <jhademels123@gmail.com>
2025-05-13 18:25:38 -0400

    feat: add auth with clerk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,20 @@
-export default function Header(){
-    return(
-        <header className="bg-zinc-600 w-full h-24">
-            header
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+export default function Header() {
+    return (
+        <header className="bg-zinc-600 w-full h-12 flex items-center justify-between
+        text-white px-2">
+            <h1>Header</h1>
+            <section>
+                <SignedOut>
+                    <div className="bg-primary p-1 px-4 font-semibold rounded-xl">
+                        <SignInButton />
+                    </div>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </section>
         </header>
     )
 }
