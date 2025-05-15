adda15eedcdf5276ea919c3bc9094f0ef147c463
Jalter-ego <jhademels123@gmail.com>
2025-05-15 06:17:40 -0400

    feat: add demo fetch login

--- src/hooks/SyncUserWithBackend.ts
+++ src/hooks/SyncUserWithBackend.ts
@@ -2,33 +2,57 @@ import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import { toast } from "sonner";
 
+interface SignInDto {
+  email: string;
+  password: string | null;
+  auth_provider: string;
+}
+
 export default function SyncUserWithBackend() {
-    const { user } = useUser();
+  const { user } = useUser();
 
-    useEffect(() => {
-        if (user) {
-            console.log(user);
-            
-            const email = user.primaryEmailAddress?.emailAddress;
-            const name = user.fullName || `${user.firstName} ${user.lastName}`;
-            console.log(email);
-            console.log(name);
-            
-            
-            //marca para no repetir
-            const alreadySynced = localStorage.getItem("user_synced");
+  const fetchSignIn = async (signInDto: SignInDto) => {
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signInDto),
+      });
 
-            if (!alreadySynced) {
-                try {
-                    //const response = await fetch(input)
-                    toast("user guardado")
-                    localStorage.setItem("user_synced", "true");
-                } catch (error) {
-                    toast.error(`Error: ${(error instanceof Error ? error.message : String(error))}`);
-                }
-            }
+      const result = await response.json()
+      console.log(result);
+      toast("user guardado");
+      localStorage.setItem("user_synced", "true");
+    } catch (error) {
+      toast.error(
+        `Error: ${error instanceof Error ? error.message : String(error)}`
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+
+      const email = user.primaryEmailAddress?.emailAddress;
+      const name = user.fullName || `${user.firstName} ${user.lastName}`;
+      console.log(email);
+      console.log(name);
+      //marca para no repetir
+      const alreadySynced = localStorage.getItem("user_synced");
+
+      if (!alreadySynced) {
+        const signInDto: SignInDto = {
+            email: email||'',
+            password: '',
+            auth_provider: 'google'
         }
-    }, [user]);
+        fetchSignIn(signInDto);
+      }
+    }
+  }, [user]);
 
-    return null;
+  return null;
 }
