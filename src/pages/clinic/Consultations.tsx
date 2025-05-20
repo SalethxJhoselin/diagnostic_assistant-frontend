dd26726f010258f51eb17b68ae0a463c300e4556
Jalter-ego <jhademels123@gmail.com>
2025-05-20 16:22:44 -0400

    feat: news pages and news folders

---
+++ src/pages/clinic/Consultations.tsx
@@ -0,0 +1,8 @@
+export default function Consultations(){
+    return(
+        <div>
+            aqui se mostraria las consultas,puede ser una tabla o ya vean su creatividad, con filtros,
+            busqueda
+        </div>
+    )
+}
---
+++ src/pages/clinic/Patients.tsx
@@ -0,0 +1,8 @@
+export default function Patients(){
+    return(
+        <div>
+            aqui se mostraria los pacientes, una tabla normal seria, con filtros, busqueda
+            ,agregar nuevo paciente, editar sus datos y eso
+        </div>
+    )
+}
