4f738ddfcbfd6315af319b5b28fd4a9371783b2e
Jalter-ego <jhademels123@gmail.com>
2025-05-20 01:26:00 -0400

    feat: add new page for team

---
+++ src/pages/TeamOrganization.tsx
@@ -0,0 +1,8 @@
+export default function TeamOrganization(){
+    return(
+        <div>
+            aqui hacer tipo tabla donde muestre los miembros de la organizacion, 
+            tambien para agregar miembros, validar si el plan permite agregar mas miembros
+        </div>
+    )
+}
