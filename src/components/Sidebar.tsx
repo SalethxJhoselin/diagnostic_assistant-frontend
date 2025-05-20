e902086de9344f2da903c81ff21f77d68d64c540
Jalter-ego <jhademels123@gmail.com>
2025-05-20 01:25:38 -0400

    feat: update component sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,43 @@
-export default function Sidebar(){
-    return(
-        <aside className="w-[300px] h-full border-r">
-            sidebar
+import { IconHome, IconUsers } from "@/assets/icons";
+import { useOrganization } from "@/hooks/organizationContex";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+    const [select, setSelect] = useState("Home Organization");
+    const {organization} = useOrganization()
+    const labels = [
+        {
+            icon: <IconHome />,
+            title: "Home Organization",
+            to: "",
+        },
+        {
+            icon: <IconUsers />,
+            title: "Team",
+            to: "team",
+        },
+    ];
+
+    return (
+        <aside className="w-[280px] h-full border-r p-4">
+            {labels.map((label) => {
+                const isSelected = select === label.title;
+                return (
+                    <Link
+                        key={label.title}
+                        to={`/dashboard/org/${organization?.id}/${label.to}`}
+                        onClick={() => setSelect(label.title)}
+                        className={`flex items-center gap-2 py-2 px-3 my-1 rounded-lg transition-colors
+                            ${isSelected ? "bg-secondary  font-bold" : `dark:text-zinc-300 text-zinc-700 hover:bg-secondary
+                             dark:hover:text-white hover:text-black hover:font-bold font-semibold`}
+                        `}
+                    >
+                        {label.icon}
+                        <span>{label.title}</span>
+                    </Link>
+                );
+            })}
         </aside>
-    )
+    );
 }
